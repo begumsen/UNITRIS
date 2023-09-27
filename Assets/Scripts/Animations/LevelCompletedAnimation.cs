5c3d50cf263b2c5f02327ca0bfa8f47f3008851a
Begum Sen <begumsen19@ku.edu.tr>
2023-09-27 14:37:07 +0300

    Update sprites and resolve UI error

--- Assets/Scripts/Animations/LevelCompletedAnimation.cs
+++ Assets/Scripts/Animations/LevelCompletedAnimation.cs
@@ -22,7 +22,7 @@ public class LevelCompletedAnimation : BaseAnimation
     {
         LeanTween.alpha(shadePanel.GetComponent<RectTransform>(), 0.8f, .5f);
         LeanTween.scale(label, new Vector3(1.2f, 1.2f, 1.2f), 1f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
-        LeanTween.moveLocal(label, new Vector3(0f, 220f, 2f), 0.3f).setDelay(1f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.moveLocal(label, new Vector3(0f, 250f, 2f), 0.3f).setDelay(1f).setEase(LeanTweenType.easeInOutCubic);
         LeanTween.scale(label, new Vector3(1f, 1f, 1f), 1f).setDelay(1f).setEase(LeanTweenType.easeInOutCubic).setOnComplete(LevelComplete);
         
     }
